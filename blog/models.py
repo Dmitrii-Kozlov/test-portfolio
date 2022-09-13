98fd5102947d5ebd55738d8548e1724da8bdf859
Dimitrii-Kozlov <demetra_n@front.ru>
2022-09-13 18:55:34 +0300

    create blog model, template, view; create base template base.html

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
+from statistics import mode
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Blog
 
 def all_blog(request):
-    return render(request, 'blog/all_blog.html')
+    blogs = Blog.objects.all()
+    context = {
+        'blogs': blogs
+    }
+    return render(request, 'blog/all_blog.html', context)
