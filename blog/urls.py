f75bf59d12c6ece00bb1d69a5855f11ca959775c
Dimitrii-Kozlov <demetra_n@front.ru>
2022-09-14 18:35:44 +0300

    create detail page for blog, add navbar in base.html

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import all_blog
+from .views import all_blog, detail
+
+app_name = 'blog'
 
 urlpatterns = [
-    path('', all_blog, name='all_blog')
+    path('', all_blog, name='all_blog'),
+    path('<int:blog_id>', detail, name='detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 def all_blog(request):
@@ -7,3 +7,10 @@ def all_blog(request):
         'blogs': blogs
     }
     return render(request, 'blog/all_blog.html', context)
+
+def detail(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    context = {
+        'blog': blog
+    }
+    return render(request, 'blog/detail.html', context)
